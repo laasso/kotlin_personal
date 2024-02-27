c7953b825f6f9b2e8401545a8b9a3d021cc0b63a
laasso <nrubialesguerrero@gmail.com>
2024-02-27 11:42:59 +0100

    calc

--- calculadora_basica/calc.kt
+++ calculadora_basica/calc.kt
@@ -1,4 +1,23 @@
 fun main(){
-    val reader = Scanner(System.`in`)
-    print("Enter a number: ")
+    resultat()
+}
+
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+fun resultat() {
+    val num3 = llegir()
+    val num4 = llegir2()
+    println(sum(num3,num4))
+}
+
+fun llegir() :Int{
+	val num1: Int = 10
+    return num1
+}
+
+fun llegir2() :Int{
+   	val num2: Int = 5
+    return num2
 }
