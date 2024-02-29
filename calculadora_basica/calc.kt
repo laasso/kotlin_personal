6a0c2766f11526f91edd3a74ac96eb7b3c4c74c4
laasso <nrubialesguerrero@gmail.com>
2024-02-29 08:39:25 +0100

    final calculadora cutre

--- calculadora_basica/calc.kt
+++ calculadora_basica/calc.kt
@@ -1,23 +1,68 @@
 fun main(){
-    resultat()
+    escollir()
 }
+
+fun
+ escollir() {
+    println("1 Sumar 2 Dividir 3 Restar 4   Multiplicar")
+    val eleccio = readLine()!!.toInt()
+    if (eleccio == 1) {
+        resultat_suma()
+    }
+        else if (eleccio == 2) {
+            resultat_div()
+        }   
+        else if (eleccio == 3) {
+            resultat_resta()
+        }
+        else if (eleccio == 4) {
+            resultat_mult()
+        }
+    }
 
 fun sum(a: Int, b: Int): Int {
     return a + b
 }
 
-fun resultat() {
-    val num3 = llegir()
-    val num4 = llegir2()
-    println(sum(num3,num4))
+fun resultat_suma() {
+    val num1 = llegir()
+    val num2 = llegir()
+    println(sum(num1,num2))
+}
+
+fun div(a: Int, b: Int): Int {
+    return a / b
+}
+
+fun resultat_div() {
+    val num1 = llegir()
+    val num2 = llegir()
+    if (num2 != 0) {
+        println(div(num1,num2))
+        }
+}
+
+fun resta(a: Int, b: Int): Int {
+    return a - b
+}
+fun resultat_resta() {
+    val num1 = llegir()
+    val num2 = llegir()
+    println(resta(num1,num2))
+}
+
+fun multiplicar(a: Int, b: Int): Int {
+    return a * b
+}
+fun resultat_mult() {
+    val num1 = llegir()
+    val num2 = llegir()
+    println(multiplicar(num1,num2))
 }
 
 fun llegir() :Int{
-	val num1: Int = 10
-    return num1
+    println("Ingresa el Valor")
+    val valor = readLine()!!.toInt()    
+    return valor
 }
 
-fun llegir2() :Int{
-   	val num2: Int = 5
-    return num2
-}
