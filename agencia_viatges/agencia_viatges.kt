d6dfe4935ec0e79f5598b5a7a4da588000b7706b
laasso <nrubialesguerrero@gmail.com>
2024-03-04 13:31:15 +0100

    Informacio Usuari

--- agencia_viatges/agencia_viatges.kt
+++ agencia_viatges/agencia_viatges.kt
@@ -1,9 +1,10 @@
+
+
+import kotlin.arrayOf
 fun eleccion() {
         interficieUsuari()
         gestioViatges()
         clientReserva()
-        
-
     }
 
 fun gestioViatges() {
@@ -15,28 +16,65 @@ fun clientReserva () {
 }
 
 fun interficieUsuari() {
-    mostrarDestinacio()
+    println("1.Registrar-se")
+    println("2.Mostrar Destinacions")
+    println("3.Reservar")
+    println("4.Informacio Reserves")
+    val eleccio = readLine()!!.toInt()
+    if (eleccio == 1) {
+        informacioPersonal()
+    }
+        else if (eleccio == 2) {
+            mostrarDestinacio()
+            
+        }   
+        else if (eleccio == 3) {
+            clientReserva()
+        }
+        else if (eleccio == 4) {
+            mostrarDestinacio()
+        }
+    }
 
-}
+
+
 
 fun main(){
     eleccion()
 }
 
 fun mostrarDestinacio() {
+    var viatges = declararViatges()
     println("Les destinacions disponibles son:")
-    declararViatges()
+    for (desti in viatges) {
+        println(desti[0])
+    }
+    
 }
 
 fun informacioPersonal() {
-    print("1")
-}
+    println("Nom:")
+    val nom = readLine()!!
+    println("Cogonom:")
 
-fun declararViatges(){
-    val viatge = arrayOf("Barcelona", 2025, "December"," 2 Hours")
-    val viatge2 = arrayOf("Granada", 2024, "August"," 1 Hours")
-    val viatge3 = arrayOf("Madrid", 2024, "May"," 3 Hours")
+    val cognom = readLine()!!
+    println("Telefon:")
+
+    val numero = readLine()!!.toInt()
+    println(" ")
+    println("Aquestes son les teves dades?")
+    println("Nom: $nom")
+    println("Cogonom: $cognom")
+    println("Telefon: $numero")
+
+
+
+    }
+
+fun declararViatges(): MutableList<Array<String>> {
+    val viatge = arrayOf("Barcelona", "2025", "December"," 2 Hours")
+    val viatge2 = arrayOf("Granada", "2024", "August"," 1 Hours")
+    val viatge3 = arrayOf("Madrid", "2024", "May"," 3 Hours")
     var viatgesLlista = mutableListOf(viatge, viatge2, viatge3)
-    print(viatgesLlista.toString())
-
+    return viatgesLlista
 }
